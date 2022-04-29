88e141633350f5cf4c7eca6fe04a3a0a6a2d4e62
Weixin Deng <weixin@cs.washington.edu>
2022-04-29 14:10:51 -0700

    Add install-go.sh

---
+++ install-go.sh
@@ -0,0 +1,7 @@
+cd
+wget https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.18.1.linux-amd64.tar.gz
+bash
+export PATH=$PATH:/usr/local/go/bin
+go version
