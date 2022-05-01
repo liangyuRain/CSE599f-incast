71976246a8d52f3da7acf527077099d26ba00d53
liangyuRain <liangyu@cs.washington.edu>
2022-04-30 22:37:10 -0700

    kill unique SSH_HOSTs

--- kill.sh
+++ kill.sh
@@ -2,13 +2,16 @@
 
 CONFIG="servers.config"
 
+SSH_HOSTs=""
 while IFS= read -r line
 do
   tokens=( $line )
-  SSH_HOST=${tokens[0]}
-  HOST=${tokens[1]}
-  PORT=${tokens[2]}
-
-  echo "Killing server on ${SSH_HOST} at ${HOST}:${PORT}..."
-  ssh -n "${SSH_HOST}" "tmux kill-server"
+  SSH_HOSTs="${SSH_HOSTs}${tokens[0]},"
 done < "${CONFIG}"
+SSH_HOSTs=$(echo "${SSH_HOSTs%,*}" | tr ',' '\n' | sort -u)
+
+while IFS= read -r line
+do
+  echo "Killing servers on ${line}..."
+  ssh -n "${line}" "tmux kill-server" || echo "No server running on ${line}"
+done <<< "${SSH_HOSTs}"
