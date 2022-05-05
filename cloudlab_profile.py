46860602aa076d4b67eb2cbd5726337613ccb08e
Liangyu <liangyu@cs.washington.edu>
2022-05-04 20:41:27 -0700

    switch added (uniform bandwidth)

--- cloudlab_profile.py
+++ cloudlab_profile.py
@@ -15,12 +15,17 @@ import geni.urn
 # Describe the parameter(s) this profile script can accept.
 # geni.portal.context.defineParameter( "NUM_DC", "number of datacenters", geni.portal.ParameterType.INTEGER, 2)
 
-geni.portal.context.defineParameter("NUM_WORKERS", "number of total workers", geni.portal.ParameterType.INTEGER, 5)
-geni.portal.context.defineParameter("INTRA_BW", "intra datacenter bandwidth in kbps", geni.portal.ParameterType.INTEGER,
-                                    1000000)
+geni.portal.context.defineParameter("NUM_WORKERS", "number of total workers", geni.portal.ParameterType.INTEGER, 9)
+geni.portal.context.defineParameter("CLIENT_BW", "client bandwidth in kbps", geni.portal.ParameterType.INTEGER,
+                                    1000)
+geni.portal.context.defineParameter("BACKEND_BW", "backend bandwidth in kbps", geni.portal.ParameterType.INTEGER,
+                                    10000000)
 
 geni.portal.context.defineParameter("size_mnt", "The size for /mnt (GB)", geni.portal.ParameterType.INTEGER, 80)
 geni.portal.context.defineParameter("size_home", "The size for /users (GB)", geni.portal.ParameterType.INTEGER, 15)
+
+geni.portal.context.defineParameter("phystype", "Switch type", geni.portal.ParameterType.STRING, "dell-s4048", 
+                                    [('mlnx-sn2410', 'Mellanox SN2410'), ('dell-s4048',  'Dell S4048')])
 
 # Retrieve the values the user specifies during instantiation.
 params = geni.portal.context.bindParameters()
@@ -28,7 +33,8 @@ params = geni.portal.context.bindParameters()
 NUM_WORKERS = params.NUM_WORKERS
 # NUM_DC = NUM_WORKERS
 
-INTRA_BW = params.INTRA_BW
+CLIENT_BW = params.CLIENT_BW
+BACKEND_BW = params.BACKEND_BW
 
 size_mnt = params.size_mnt
 size_home = params.size_home
@@ -47,9 +53,9 @@ request = geni.portal.context.makeRequestRSpec()
 hosts = [None for _ in range(NUM_WORKERS)]
 
 
-# lan
-lan = request.LAN("lan")
-# lan.link_multiplexing = True
+# switch
+sw = request.Switch("switch");
+sw.hardware_type = params.phystype
 
 
 # Workers
@@ -62,15 +68,14 @@ for i in range(NUM_WORKERS):
     bs_home = hosts[i].Blockstore(host_name + '1', "/users")
     bs_home.size = str(size_home) + 'GB'
 
-    hosts[i].hardware_type = "c6420"
-
-    # interface and LAN
-    ip_worker = geni.rspec.pg.IPv4Address("10.0.0." + str(i + 1), NET_MASK)
-    if_worker = hosts[i].addInterface("if" + str(i + 1), ip_worker)
-    if_worker.bandwidth = INTRA_BW
-    lan.addInterface(if_worker)
-
-
+    # interface and Switch
+    if_worker = hosts[i].addInterface()
+    if_worker.addAddress(geni.rspec.pg.IPv4Address("10.0.0." + str(i + 1), NET_MASK))
+    if_sw = sw.addInterface()
+    
+    link = request.L1Link("link" + str(i + 1))
+    link.addInterface(if_worker)
+    link.addInterface(if_sw)
 
 # Print the RSpec to the enclosing page.
 geni.portal.context.printRequestRSpec()
