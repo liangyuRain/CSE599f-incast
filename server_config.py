1b83e2543f623f9fbf59be90d7daf03cdf77646d
Liangyu <liangyu@cs.washington.edu>
2022-05-07 15:40:18 -0700

    server_config round robin

--- server_config.py
+++ server_config.py
@@ -5,6 +5,6 @@ num_per_server = 5
 if __name__ == "__main__":
     nums = [5, 8, 2, 15, 6, 17, 20, 26]
     with open("servers.config", "w") as f:
-        for i in range(len(nums)):
-            for j in range(num_per_server):
+        for j in range(num_per_server):
+            for i in range(len(nums)):
                 f.write(f"liangyu@hp{nums[i]:03}.utah.cloudlab.us 10.0.0.{base_ip_suffix + i} {base_port + j}\n")
