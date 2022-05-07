2959121b865b57315cef10b3ff539c6fa86ecd00
Liangyu <liangyu@cs.washington.edu>
2022-05-07 15:36:38 -0700

    fixup! bash script to limit client bandwidth

--- run_wondershaper.sh
+++ run_wondershaper.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-LIMIT=$1
-
-let "LIMIT_IN_KBps=$LIMIT*1000/8/1024"
+LIMIT=$1 #Kbps
 
 if [ ! -d "wondershaper" ]; then
   git clone https://github.com/magnific0/wondershaper.git
@@ -11,9 +9,9 @@ if [ ! -d "wondershaper" ]; then
 fi
 
 if [ $LIMIT -gt 0 ]; then
-  echo "Limiting downlink speed to ${LIMIT}Kbps (${LIMIT_IN_KBps}KB/s) ..."
-  sudo wondershaper -a eno50 -d $LIMIT_IN_KBps
-  echo "Limiting downlink speed to ${LIMIT}Kbps (${LIMIT_IN_KBps}KB/s) ... done"
+  echo "Limiting downlink speed to ${LIMIT}Kbps ..."
+  sudo wondershaper -a eno50 -d $LIMIT
+  echo "Limiting downlink speed to ${LIMIT}Kbps ... done"
 else
   echo "Clear downlink speed limit ..."
   sudo wondershaper -c -a eno50
--- set_bw_limit.sh
+++ set_bw_limit.sh
@@ -3,7 +3,7 @@
 RUN_SCRIPT="run_wondershaper.sh"
 
 SSH_HOST=$1
-LIMIT=$2
+LIMIT=$2 # Kbps
 
 scp -o "StrictHostKeyChecking=no" "${RUN_SCRIPT}" "${SSH_HOST}:~/"
 ssh -n -o "StrictHostKeyChecking=no" "${SSH_HOST}" "bash ${RUN_SCRIPT} ${LIMIT}"
