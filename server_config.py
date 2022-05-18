3a771ff505d0935177eecb29310b59c099ada35e
liangyuRain <liangyu@cs.washington.edu>
2022-05-17 23:38:46 -0700

    server_config script template

--- server_config.py
+++ server_config.py
@@ -1,10 +1,14 @@
-base_ip_suffix = 2
+server_nums = [] # server nums
+client_server = None # client server num
+
+base_ip_suffix = 1
 base_port = 3000
-num_per_server = 5
+num_per_server = 100
 
 if __name__ == "__main__":
-    nums = [5, 8, 2, 15, 6, 17, 20, 26]
     with open("servers.config", "w") as f:
         for j in range(num_per_server):
-            for i in range(len(nums)):
-                f.write(f"liangyu@hp{nums[i]:03}.utah.cloudlab.us 10.0.0.{base_ip_suffix + i} {base_port + j}\n")
+            for i in range(len(server_nums)):
+                if server_nums[i] == client_server:
+                    continue
+                f.write(f"liangyu@hp{server_nums[i]:03}.utah.cloudlab.us 10.0.0.{base_ip_suffix + i} {base_port + j}\n")
